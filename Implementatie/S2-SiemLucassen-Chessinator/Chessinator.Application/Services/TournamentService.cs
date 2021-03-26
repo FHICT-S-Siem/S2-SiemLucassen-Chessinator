245374168fa4ffc79212c9f7c11f6ecbd6516182
Siem Lucassen <siem228@hotmail.com>
2021-03-26 16:47:28 +0100

    Changed file location

---
+++ Implementatie/S2-SiemLucassen-Chessinator/Chessinator.Application/Services/TournamentService.cs
@@ -0,0 +1,14 @@
+﻿using Chessinator.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chessinator.Application.Services
+{
+    public class TournamentService : ITournamentService
+    {
+
+    }
+}
---
+++ Implementatie/S2-SiemLucassen-Chessinator/DataAccessLibrary/IPeopleData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface IPeopleData
+    {
+        public Task<List<PersonModel>> GetPeople();
+        public Task InsertPerson(PersonModel person);
+    }
+}
